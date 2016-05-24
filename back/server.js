c1ea5ee3bdaa52187dbdd035933ac9f0ad920537
rodrigogiraldi <rgiraldisilva@gmail.com>
2016-05-24 18:42:00 -0300

    started connection mongo

--- back/server.js
+++ back/server.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var mongodb = require('mongodb');
 
-var MongoClient = mongodb.MongoClient;
+//MONGO
 var url = 'mongodb://localhost:27017/whattodo';
-
-var app = express();
-app.get('/thing/listall', function(req, res){
-    res.send("list");
+    
+var MongoClient = mongodb.MongoClient;
+MongoClient.connect(url, function(err, db){
+    var collection = db.collection('users');
+    var documents = collection.find().toArray(function(err, results){
+       console.log(results); 
+    });
 });
+
+// app.get('/thing/listall', function(req, res){
+//     res.send("list");
+// });
+
+//EXPRESS
+var app = express();
+
+app.post('/user/check', function(req, res){
+    res.send({msg: "ok"});
+    console.log("user check");
+    // console.log(req);
+})
 
 var port = 8081;
 var server = app.listen(port, function(){
