e1ba3d40a70f9a55225ede0e7b266984bb41eb6d
rodrigogiraldi <rgiraldisilva@gmail.com>
2016-05-31 11:13:11 -0300

    started creating session to login

--- back/server.js
+++ back/server.js
@@ -1,3 +1,4 @@
+var cookieSession = require('cookie-session');
 var express = require('express');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
@@ -7,9 +8,21 @@ var url = 'mongodb://localhost:27017/whattodo';
 
 var app = express();
 
-app.get('/', function(req, res){
-    res.send({msg: "ok"});
-})
+app.use(cookieSession({
+    name: 'session',
+    keys: ['kasjhd3j7nbfasjh']
+}))
+
+app.use("/static", express.static("../front/web/public"));
+
+app.get('/', function (req, res) {
+    if (req.session.user == undefined) {
+        res.redirect("/static/pages/login.html");
+    }
+    else {
+        res.send("Logado");
+    }
+});
 
 var port = 8002;
 app.listen(port);
