9cc32cac5fee3292dd57661bccd69c087ed29104
rodrigogiraldi <rgiraldisilva@gmail.com>
2016-06-10 09:19:41 -0300

    chat done

--- front/app/whattodo/www/js/controllers/chat.js
+++ front/app/whattodo/www/js/controllers/chat.js
@@ -1,4 +1,16 @@
 app.controller("ChatCtrl", function ($scope, $interval, $http, $cookies) {
+    $scope.sendMessage = function(){
+        if ($scope.message !== undefined && $scope.message !== ""){
+            $http
+                .post(host + "/chat/sendmessage", {user: $cookies.get("email"), message: $scope.message})
+                .then(function(res){
+                    $scope.message = "";
+                    $scope.listAll();
+                    // console.log(res.data);
+                });
+        }
+    };    
+    
     $scope.listAll = function () {
         $http
             .get(host + "/chat/listall")
@@ -10,4 +22,8 @@ app.controller("ChatCtrl", function ($scope, $interval, $http, $cookies) {
                 }
             });
     };
+    
+    $interval(function(){
+        $scope.listAll();
+    }, 10000);    
 });
